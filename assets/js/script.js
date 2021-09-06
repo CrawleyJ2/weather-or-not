9e3535b21e050a659064bf10cb2c6b4d38af07ee
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-06 17:55:03 -0400

    api call pending testing

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const pullWeather = function(city) {
+  fetch('https://api.openweathermap.org/geo/1.0/direct?q='+ city +'&limit=1&appid=e89a8e141812e6ab1807eaf3ed0ffd77')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    var lat = response[0].lat;
+    var lon = response[0].lon;
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude={part}&appid=e89a8e141812e6ab1807eaf3ed0ffd77')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      var returnedStats = [
+        response.daily[0].temp.day,
+        response.daily[0].wind_speed,
+        response.daily[0].humidity,
+        response.daily[0].uvi,
+        response.daily[0].weather[0].icon
+      ]
+    })
+  })
+};
+
+
+
