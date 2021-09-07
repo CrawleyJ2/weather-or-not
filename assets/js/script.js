e375434b6a9f3c38b2b798c3fbde78e2739cda27
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-07 16:44:23 -0400

    updated UV index display after error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,8 @@ var pullWeather = function(city) {
       saveCity();
       var lat = data[0].lat;
       var lon = data[0].lon;
-      fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=e89a8e141812e6ab1807eaf3ed0ffd77').then(function(response) {
+      
+      return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=e89a8e141812e6ab1807eaf3ed0ffd77').then(function(response) {
         response.json().then(function(data) {
           console.log(data);
           var cityTitleEl = document.querySelector('#city-date');
@@ -72,17 +73,41 @@ var pullWeather = function(city) {
           var currentHumidityEl = document.querySelector('#humidity');
           currentHumidityEl.innerHTML = data.current.humidity + '<span>&#37;</span>';
           var currentUVIndexEl = document.querySelector('#uv-index');
-          currentUVIndexEl.innerHTML = Math.floor(data.current.uvi);
+          currentUVIndexEl.innerHTML = (data.current.uvi);
           // handler for displaying severity of uv index
           if (parseInt(currentUVIndexEl.textContent) < 3) {
-            currentUVIndexEl.setAttribute('id', 'low');
-          } else if (parseInt(currentUVIndexEl.textContent) > 7.99) {
-            currentUVIndexEl.setAttribute('id', 'severe');
+            currentUVIndexEl.setAttribute('class', 'low');
+          } else if (parseInt(currentUVIndexEl.textContent) > 8) {
+            currentUVIndexEl.setAttribute('class', 'severe');
           } else {
-            currentUVIndexEl.setAttribute('id', 'moderate');
+            currentUVIndexEl.setAttribute('class', 'moderate');
           }
 
-          // set up for 5 day forecart
+          // // set up for 5 day forecas
+          // var dayForecast = function(dayCard, dayIndex) {
+          // // date info
+          // var forecastDateEl = document.querySelector('#'+ dayCard +'-date');
+          // forecastDateEl.innerHTML = moment.unix(data.daily[dayIndex].dt).format('MM/DD/YYYY');
+          // // icon info
+          // var forecastIconEl = document.querySelector('#'+ dayCard +'-icon');
+          // forecastIconEl.innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.daily[dayIndex].weather[0].icon + '.png">'
+          // // temp info
+          // var forecastTempEl = document.querySelector('#'+ dayCard +'-temp');
+          // forecastTempEl.innerHTML = data.daily[dayIndex].temp.max + '<span>&#176;</span>F';
+          // // wind info
+          // var forecastWindEl = document.querySelector('#'+ dayCard +'-wind');
+          // forecastWindEl.innerHTML = data.daily[dayIndex].wind_speed + ' MPH';
+          // // humidity info
+          // var forecastWindEl = document.querySelector('#'+ dayCard +'-humidity');
+          // forecastWindEl.innerHTML = data.daily[dayIndex].humidity + '<span>&#37;</span>';
+          // };
+          // // 5 day forecast
+          // var dayCardArr = ['day1', 'day2', 'day3', 'day4', 'day5'];
+          // var dayIndexArr = [1, 2, 3, 4, 5];
+          // dayCardArr.forEach((dayCard, index) => {
+          //     var dayIndex = dayIndexArr[index];
+          //     dayForecast(dayCard, dayIndex);
+          // });
         });
       });
     });
