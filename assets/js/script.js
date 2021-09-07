3c1beef5311be97b958c93f7fdcb1e65732406c5
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-07 15:25:52 -0400

    current day city info displays on page, corrected API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,23 @@ var selectedCityEl = document.querySelector('#city');
 var savedCitiesFormEl = document.querySelector('#savedFormGroup');
 //var savedCitiesSearch = document.querySelector('#savedFormGroup');
 
+// form handler for entered city
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  var city = selectedCityEl.value.trim();
+
+  pullWeather(city);
+};
+
+// form handler for saved city-repeated search
+var savedCityFormSubmit = function(event) {
+  event.preventDefault();
+  var savedCity = event.target.textContent;
+  console.log(savedCity);
+
+  pullWeather(savedCity);
+};
+
 // event listener - search form submission
 searchFormEl.addEventListener('submit', formSubmitHandler);
 // event listener - saved city submission
@@ -17,7 +34,7 @@ var getSavedCities = function() {
     cityList = savedCities;
   }
 
-  cityList.forEach((city, index) => {
+  savedCities.forEach((city, index) => {
     var formList = document.querySelector('#savedFormGroup');
     var cityButton = document.createElement('button');
     cityButton.setAttribute('class', 'col-12 savedCityBtn');
@@ -27,25 +44,11 @@ var getSavedCities = function() {
   });
 };
 
-getSavedCities();
-
 // send saved cities to localstorage
 function saveCity () {
   localStorage.setItem('cities', JSON.stringify(savedCities));
   document.querySelector('#savedFormGroup').innerHTML = '<label class="title">Saved Cities</label>';
   getSavedCities();
-};
-
-// form handler for entered city
-var formSubmitHandler = function(event) {
-  event.preventDefault();
-  var city = selectedCityEl.nodeValue.trim();
-
-  pullWeather(city);
-};
-// form handler for saved city-repeated search
-var savedCityFormSubmit = function(event) {
-
 };
 
 // function to get weather from api
@@ -57,9 +60,23 @@ var pullWeather = function(city) {
       saveCity();
       var lat = data[0].lat;
       var lon = data[0].lon;
-      fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=e89a8e141812e6ab1807eaf3ed0ffd77').then(function(response) {
+      fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=e89a8e141812e6ab1807eaf3ed0ffd77').then(function(response) {
         response.json().then(function(data) {
           console.log(data);
+          var cityTitleEl = document.querySelector('#city-date');
+          cityTitleEl.innerHTML = city + " " + moment.unix(data.current.dt).format('MM/DD/YYYY') + ' <img src="https://openweathermap.org/img/wn/' + data.current.weather[0].icon + '.png">';
+          // city current temp info
+          var currentTempEl = document.querySelector('#temp');
+          currentTempEl.innerHTML = data.current.temp + '<span>&#176;</span>F';
+          // city current wind info
+          var currentWindEl = document.querySelector('#wind');
+          currentWindEl.innerHTML = data.current.wind_speed + ' MPH';
+          // city current humidity info
+          var currentHumidityEl = document.querySelector('#humidity');
+          currentHumidityEl.innerHTML = data.current.humidity + '<span>&#37;</span>';
+          // city current uv index info
+          var currentUVIndexEl = document.querySelector('#uv-index');
+          currentUVIndexEl.innerHTML = data.current.uvi;
         });
       });
     });
