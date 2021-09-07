130e22ec70cc5a494c8c6dc5d7119d6b1f485beb
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-06 22:27:04 -0400

    reworking script file and updated html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,41 @@
-const savedCityData = localStorage.getItem('cities');
+var savedCities = [];
+var searchFormEl = document.querySelector('#search-form');
+var selectedCityEl = document.querySelector('#city');
+var savedCitiesFormEl = document.querySelector('#savedFormGroup');
 
+var getSavedCities = function() {
+  var savedCities = JSON.parse(localStorage.getItem('cities'));
 
-const pullWeather = function(city) {
-  fetch('https://api.openweathermap.org/geo/1.0/direct?q='+ city +'&limit=1&appid=e89a8e141812e6ab1807eaf3ed0ffd77')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    var lat = response[0].lat;
-    var lon = response[0].lon;
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude={part}&appid=e89a8e141812e6ab1807eaf3ed0ffd77')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      var returnedStats = [
-        response.daily[0].temp.day,
-        response.daily[0].wind_speed,
-        response.daily[0].humidity,
-        response.daily[0].uvi,
-        response.daily[0].weather[0].icon
-      ]
-    })
-  })
+  if (savedCities !== null) {
+    cityList = savedCities;
+  }
+
+  cityList.forEach((city, index) => {
+    var formList = document.querySelector('#savedFormGroup');
+    var cityButton = document.createElement('button');
+    cityButton.setAttribute('class', 'col-12 savedCityBtn');
+    cityButton.setAttribute('id', city + '-saveBtn');
+    cityButton.textContent = city;
+    formList.appendChild(cityButton); 
+  });
+};
+
+getSavedCities();
+
+// send saved cities to localstorage
+function saveCity () {
+  localStorage.setItem('cities', JSON.stringify(savedCities));
+  document.querySelector('#savedFormGroup').innerHTML = '<label class="title">Saved Cities</label>';
+  getSavedCities();
 };
 
 
+// fetch('https://api.openweathermap.org/geo/1.0/direct?q='+ city +'&limit=1&appid=e89a8e141812e6ab1807eaf3ed0ffd77')
+//   .then(function(response) {
+//     return response.json();
+//   });
 
+// fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=e89a8e141812e6ab1807eaf3ed0ffd77')
+// .then(function(response) {
+//   return response.json();
+// })
