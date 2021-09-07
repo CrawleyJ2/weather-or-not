3f07d26cecea261af7ebcecb4ae16da47f99e3b9
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-07 17:22:49 -0400

    corrected html for display of 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var savedCities = [];
 var searchFormEl = document.querySelector('#search-form');
 var selectedCityEl = document.querySelector('#city');
 var savedCitiesFormEl = document.querySelector('#savedFormGroup');
-//var savedCitiesSearch = document.querySelector('#savedFormGroup');
+var savedCitiesSearch = document.querySelector('#savedFormGroup');
 
 // form handler for entered city
 var formSubmitHandler = function(event) {
@@ -83,31 +83,31 @@ var pullWeather = function(city) {
             currentUVIndexEl.setAttribute('class', 'moderate');
           }
 
-          // // set up for 5 day forecas
-          // var dayForecast = function(dayCard, dayIndex) {
-          // // date info
-          // var forecastDateEl = document.querySelector('#'+ dayCard +'-date');
-          // forecastDateEl.innerHTML = moment.unix(data.daily[dayIndex].dt).format('MM/DD/YYYY');
-          // // icon info
-          // var forecastIconEl = document.querySelector('#'+ dayCard +'-icon');
-          // forecastIconEl.innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.daily[dayIndex].weather[0].icon + '.png">'
-          // // temp info
-          // var forecastTempEl = document.querySelector('#'+ dayCard +'-temp');
-          // forecastTempEl.innerHTML = data.daily[dayIndex].temp.max + '<span>&#176;</span>F';
-          // // wind info
-          // var forecastWindEl = document.querySelector('#'+ dayCard +'-wind');
-          // forecastWindEl.innerHTML = data.daily[dayIndex].wind_speed + ' MPH';
-          // // humidity info
-          // var forecastWindEl = document.querySelector('#'+ dayCard +'-humidity');
-          // forecastWindEl.innerHTML = data.daily[dayIndex].humidity + '<span>&#37;</span>';
-          // };
-          // // 5 day forecast
-          // var dayCardArr = ['day1', 'day2', 'day3', 'day4', 'day5'];
-          // var dayIndexArr = [1, 2, 3, 4, 5];
-          // dayCardArr.forEach((dayCard, index) => {
-          //     var dayIndex = dayIndexArr[index];
-          //     dayForecast(dayCard, dayIndex);
-          // });
+          // set up for 5 day forecas
+          var dayForecast = function(dayCard, dayIndex) {
+            // date info
+            var forecastDateEl = document.querySelector('#'+ dayCard +'-date');
+            forecastDateEl.innerHTML = moment.unix(data.daily[dayIndex].dt).format('MM/DD/YYYY');
+            // icon info
+            var forecastIconEl = document.querySelector('#'+ dayCard +'-icon');
+            forecastIconEl.innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.daily[dayIndex].weather[0].icon + '.png">'
+            // temp info
+            var forecastTempEl = document.querySelector('#'+ dayCard +'-temp');
+            forecastTempEl.innerHTML = Math.floor(data.daily[dayIndex].temp.max) + '<span>&#176;</span>F';
+            // wind info
+            var forecastWindEl = document.querySelector('#'+ dayCard +'-wind');
+            forecastWindEl.innerHTML = Math.floor(data.daily[dayIndex].wind_speed) + ' MPH';
+            // humidity info
+            var forecastWindEl = document.querySelector('#'+ dayCard +'-humidity');
+            forecastWindEl.innerHTML = data.daily[dayIndex].humidity + '<span>&#37;</span>';
+          };
+          // 5 day forecast
+          var dayCardArr = ['day1', 'day2', 'day3', 'day4', 'day5'];
+          var dayIndexArr = [1, 2, 3, 4, 5];
+          dayCardArr.forEach((dayCard, index) => {
+              var dayIndex = dayIndexArr[index];
+              dayForecast(dayCard, dayIndex);
+          });
         });
       });
     });
